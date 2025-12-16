a509328081a719ca591afbf73379fb736c451ef2
Arvind carpenter <arvindcarpenter94@gmail.com>
2025-12-16 21:31:07 +0530

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand">DSMax Starline</a>
+    
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/dashboard">Dashboard</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/flats">Flats</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/payments">Payments</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/ledger">Ledger</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    </template>
