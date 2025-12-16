8a511f211b6af91c5a3b34237c8cc06a17fbf087
Arvind carpenter <arvindcarpenter94@gmail.com>
2025-12-16 21:20:20 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Navbar v-if="showNavbar" />
+
+  <main class="app-container">
+    <router-view />
+  </main>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+
+const route = useRoute()
+const showNavbar = computed(() => route.name !== 'Login')
+</script>
+
+<style>
+.app-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1.5rem;
+}
+</style>
